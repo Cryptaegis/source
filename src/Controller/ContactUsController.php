1dcad93d286be01e342644501fa8c804a74f093c
Candelier.M <58921922+ravenqueenofinsanity@users.noreply.github.com>
2024-01-10 20:53:26 +0100

    Contact Form

---
+++ src/Controller/ContactUsController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\ContactUs;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Form\ContactUsType;
+use Doctrine\ORM\EntityManagerInterface;
+
+class ContactUsController extends AbstractController
+{
+    #[Route('/contact/us', name: 'app_contact_us')]
+    public function index(Request $request, EntityManagerInterface $manager): Response
+    {
+        $contact = new ContactUs();
+       
+
+
+        $form = $this->createForm(ContactUsType::class, $contact);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contact = $form->getData();
+            $manager->persist($contact);
+            $manager->flush();
+            $this->addFlash(
+                'success',
+                'Nous vous rapellerons dans 11jours!'
+            );
+            return $this->redirectToRoute('app_contact_us');
+        }else {
+            $this->addFlash(
+                'danger',
+                $form->getErrors()
+            );
+        }
+
+
+        return $this->render('contact_us\index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
---
+++ src/Form/ContactUsType.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\ContactUs;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length; // Add this line
+use Symfony\Component\Form\FormBuilderInterface; // Add this line
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ContactUsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void // Update this line
+    {
+        // Code for building the form
+
+        $builder
+            ->add('email', EmailType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => '2',
+                    'maxlength' => '180',
+                ],
+                'label' => 'Adresse email',
+                'label_attr' => [
+                    'class' => 'form-label mt-4',
+                ],
+                'constraints' => [
+                    new Email(),
+                    new Length(['min' => 2, 'max' => 180, 'minMessage' => "minErrorMessage", 'maxMessage' => "MaxErrorMessage"]) // Update this line
+                ]
+            ])
+            ->add('Subject', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => '2',
+                    'maxlength' => '200',
+                ],
+                'label' => 'Sujet',
+                'label_attr' => [
+                    'class' => 'form-label mt-4',
+                ],
+                'constraints' => [
+                    new Length(['min' => 2, 'max' => 100, 'minMessage' => "minErrorMessage", 'maxMessage' => "MaxErrorMessage"])
+                ]
+            ])
+            ->add('message', TextareaType::class,
+                [
+                    'attr' => [
+                        'class' => 'form-control'
+                    ],
+                    'label' => 'Description',
+                    'label_attr' => [
+                        'class' => 'form-label mt-4',
+                    ],
+                    'constraints' => [
+                        new NotBlank(),
+                    ]
+                ])
+            ->add('submit', SubmitType::class);
+    }
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => ContactUs::class,
+        ]);
+    }
+}
